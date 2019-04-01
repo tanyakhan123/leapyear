37ad62b393777e80a6c87706a74e33848f6bf884
Tanya Khan <tkhan@hillers.org>
2019-04-01 15:39:02 +0000

    first commit for leap year

--- leapyear.c
+++ leapyear.c
@@ -5,28 +5,26 @@
 
 
 int year;
-
 int main()
 
 {
 
   printf("Enter a year: \n");
-
   scanf("%d", &year);
 
 
 
   if (year % 4 == 0)
 
-    printf("%d is a leap year.\n", year);
+    printf("%d is a leap year!!.\n", year);
 
   else if (year % 1 == 0)
 
-    printf("%d isn't a leap year.\n", year);
+    printf("%d oh no... not a leap year):.\n", year);
 
   else
 
-    printf("%d isn't a leap year.\n", year);
+    printf("%d not a leap year.....\n", year);
 
 
 
